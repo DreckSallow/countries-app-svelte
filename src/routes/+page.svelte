e9a1cc8bd13ada39f307507bf9411da674962e35
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2022-10-02 18:15:29 -0500

    feat: add scaffolding

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="js">
+	let name = 'Dikson es un capo';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
