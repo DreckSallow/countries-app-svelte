42452e11ce2610f83d467e479194f567c9037e2a
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2022-10-04 14:39:30 -0500

    feat(layout | landing): add init page(landing) and more scaffolding

---
+++ src/routes/+error.svelte
@@ -0,0 +1,1 @@
+<div class="p-6 bg-blue-400">Error al cargar la pagina jejeje</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { LayoutData } from './$types';
+	import '../app.css';
+	let skipPaths = ['/'];
+	let showNav = true;
+	export let data: LayoutData;
+	$: showNav = skipPaths.find((p) => p === data.pathName) ? false : true;
+</script>
+
+<main class="bg-slate-700 text-white min-h-screen relative">
+	{#if showNav}
+		<nav>
+			<a href="/">Home</a>
+			<a sveltekit:prefetch href="/countries">countries</a>
+		</nav>
+	{/if}
+	<slot />
+</main>
